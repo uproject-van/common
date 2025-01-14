341d8bd873cd1e8a96ab86716032ca51089030f2
uproject <uproject@163.com>
2025-01-14 22:50:19 +0800

    添加点击按钮和选择按钮

---
+++ Editor/Base/_AUTConfirmItem.cs
@@ -0,0 +1,71 @@
+﻿
+using UnityEngine;
+
+namespace UTPackage
+{
+    /// <summary>
+    /// 按钮基类
+    /// </summary>
+    public abstract class _AUTConfirmItem
+    {
+        public void onGUI()
+        {
+            //创建一个按钮对象
+            if (GUILayout.Button(_text, GUILayout.Height(30), GUILayout.Width(600)))
+            {
+                _dealComfirm();
+            }
+        }
+
+        /// <summary>
+        /// 显示的说明
+        /// </summary>
+        protected abstract string _text { get; }
+
+        /// <summary>
+        /// 具体的处理函数
+        /// </summary>
+        protected abstract void _dealComfirm();
+    }
+
+    /// <summary>
+    /// Toggle 基类
+    /// </summary>
+    public abstract class _AUTToggleItem 
+    {
+        protected bool _m_bIsOn;
+        private bool tempResult;
+
+        public bool isOn { get { return _m_bIsOn; } }
+
+        public _AUTToggleItem()
+        {
+            _m_bIsOn = false;
+        }
+
+        public void onGUI()
+        {
+            tempResult = GUILayout.Toggle(_m_bIsOn, _text, GUILayout.Height(30));
+            if (tempResult != _m_bIsOn)
+            {
+                _m_bIsOn = tempResult;
+                _dealTgeChg(_m_bIsOn);
+            }
+        }
+
+        protected abstract string _text { get; }
+
+
+        protected abstract void _dealTgeChg(bool _isOn);
+
+        public void setToggleValue(bool _isOn)
+        {
+            if (_m_bIsOn == _isOn)
+                return;
+
+            _m_bIsOn = _isOn;
+        }
+
+    }
+}
+
---
+++ Editor/UTExportAllItem.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace UTPackage
+{
+    /// <summary>
+    /// 全部导出按钮
+    /// </summary>
+    public class UTExportAllItem : _AUTConfirmItem
+    {
+        //导出所有的操作函数
+        private Action _m_dExportAllAction = default(Action);
+
+        protected override string _text { get { return "全 部 导 出"; } }
+
+        //处理点击
+        protected override void _dealComfirm()
+        {
+            if (_m_dExportAllAction != null) {
+                _m_dExportAllAction();
+            }
+        }
+
+        //注册点击回调
+        public void regExportFunc(Action _action)
+        {
+            if (null == _action)
+                return;
+
+            _m_dExportAllAction += _action;
+        }
+    }
+
+    //选择按钮
+    public class UTSelectItem : _AUTToggleItem
+    {
+        private string _m_sText;
+
+        private Action<bool> _m_dSelected = default(Action<bool>);
+
+        public UTSelectItem(string _text) {
+             _m_sText = _text;
+        }
+
+        public UTSelectItem(string _text, Action<bool> _action) {
+            _m_sText = _text;
+            _m_dSelected = _action;
+        }
+
+        protected override string _text { get { return _m_sText; } }
+
+        protected override void _dealTgeChg (bool _isOn) {
+
+           if(_m_dSelected != null)
+               _m_dSelected(_isOn);
+        }
+
+        //注册选择回调
+        public void regSelectFunc(Action<bool> _action)
+        {
+            if (null == _action)
+                return;
+
+            _m_dSelected += _action;
+        }
+    }
+}
+
